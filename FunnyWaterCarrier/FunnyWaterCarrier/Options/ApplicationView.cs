8e0d0a1bc6fd1ff59cedab372142bbf3b62b5f49
Lilia <lil25mrch@gmail.com>
2020-05-22 16:16:26 +0300

    name refactoring

--- FunnyWaterCarrier/FunnyWaterCarrier/Options/ApplicationView.cs
+++ FunnyWaterCarrier/FunnyWaterCarrier/Options/ApplicationView.cs
@@ -1,56 +1,42 @@
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Configuration;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using FunnyWaterCarrier.Commands;
 using FunnyWaterCarrier.DAL.Entities;
-using FunnyWaterCarrier.DAL.Factories;
-using FunnyWaterCarrier.DAL.Factories.Contracts;
-using FunnyWaterCarrier.DAL.Providers;
 using FunnyWaterCarrier.DAL.Providers.Contracts;
 
 namespace FunnyWaterCarrier.Options {
     public class ApplicationView<TEntity> where TEntity : BaseEntity, new() {
         private readonly IDbProvider<TEntity> _dbProvider;
+
+        private AddCommand _addCommand;
+
+        private UpdateCommand _updateUpdateCommand;
+
         public ApplicationView(IDbProvider<TEntity> dbProvider) {
             _dbProvider = dbProvider;
-            Entities = new ObservableCollection<TEntity>(_dbProvider.GetAll());;
+            Entities = new ObservableCollection<TEntity>(_dbProvider.GetAll());
         }
-  
+
         public ObservableCollection<TEntity> Entities { get; set; }
 
-        private TEntity _selectedEntity;
-        public TEntity SelectedEntity {
-            get => _selectedEntity;
-            set => _selectedEntity = value;
-        }
+        public TEntity SelectedEntity { get; set; }
 
-        private AddCommand _addUpdateCommand;
-        public AddCommand AddUpdateCommand
-        {
-            get
-            {
-                return _addUpdateCommand ??
-                       (_addUpdateCommand = new AddCommand(obj => {
-                           TEntity entity = new TEntity();
-                           int entityId = _dbProvider.Insert(entity);
-                           entity.Id = entityId;
-                           Entities.Add(entity);
-                       }));
+        public AddCommand AddCommand {
+            get {
+                return _addCommand ?? (_addCommand = new AddCommand(obj => {
+                    TEntity entity = new TEntity();
+                    int entityId = _dbProvider.Insert(entity);
+                    entity.Id = entityId;
+                    Entities.Add(entity);
+                }));
             }
         }
-        
-        private UpdateCommand _updateUpdateCommand;
-        public UpdateCommand UpdateCommand
-        {
-            get
-            {
-                return _updateUpdateCommand ??
-                       (_updateUpdateCommand = new UpdateCommand(obj => {
-                           TEntity entity = obj as TEntity;
-                           _dbProvider.Update(entity);
-                       }));
+
+        public UpdateCommand UpdateCommand {
+            get {
+                return _updateUpdateCommand ?? (_updateUpdateCommand = new UpdateCommand(obj => {
+                    TEntity entity = obj as TEntity;
+                    _dbProvider.Update(entity);
+                }));
             }
         }
     }
